0dc8225bdb86ea1adb3a8212e9315e2d9ea6cb53
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-18 21:51:17 -0300

    Set up BaseEntity

---
+++ src/main/java/org/janus/shared/domain/base/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package org.janus.shared.domain.base.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity {
+
+    private UUID id;
+    private Long version;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+    private OffsetDateTime deletedAt;
+
+}
