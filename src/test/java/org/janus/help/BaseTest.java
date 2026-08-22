6f6c0285bc09b93c404360dc1d49543b67a57999
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-22 10:40:21 -0300

    New method createSampleInbox and initInbox

--- src/test/java/org/janus/help/BaseTest.java
+++ src/test/java/org/janus/help/BaseTest.java
@@ -6,6 +6,9 @@ import org.janus.modules.identity.domain.entity.UserCredentialsEntity;
 import org.janus.modules.identity.domain.entity.UserEntity;
 import org.janus.modules.identity.ports.out.UserCredentialRepository;
 import org.janus.modules.identity.ports.out.UserRepository;
+import org.janus.modules.reliability.domain.entity.InboxEntity;
+import org.janus.modules.reliability.port.out.InboxRepository;
+import org.janus.shared.domain.enums.InboxStatusEnum;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.time.OffsetDateTime;
@@ -20,12 +23,32 @@ public class BaseTest {
     protected UserCredentialRepository userCredentialRepository;
 
     @Inject
+    protected InboxRepository inboxRepository;
+
+    @Inject
     protected ObjectMapper mapper;
 
     @BeforeEach
     void setup() {
+        this.inboxRepository.deleteAll();
         this.userCredentialRepository.deleteAll();
         this.userRepository.deleteAll();
+    }
+
+    protected InboxEntity createSampleInbox() {
+        InboxEntity inbox = new InboxEntity();
+        inbox.setId(UUID.randomUUID());
+        inbox.setMessageKey("msg-" + UUID.randomUUID());
+        inbox.setConsumerGroup("order-processing-group");
+        inbox.setStatus(InboxStatusEnum.PROCESSING);
+        inbox.setResponsePayload("{\"status\":\"success\"}");
+        inbox.setResponseCode(200);
+        return inbox;
+    }
+
+    protected InboxEntity initInbox() {
+        InboxEntity inbox = createSampleInbox();
+        return inboxRepository.insert(inbox);
     }
 
     protected UserCredentialsEntity createSampleCredentials(UUID userId) {
