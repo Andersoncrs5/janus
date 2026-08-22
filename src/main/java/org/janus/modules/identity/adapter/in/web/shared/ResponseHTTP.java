0fe691568647192b36f773fbc7999b6588001835
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-22 10:35:08 -0300

    Added ResponseHTTP

---
+++ src/main/java/org/janus/modules/identity/adapter/in/web/shared/ResponseHTTP.java
@@ -0,0 +1,58 @@
+package org.janus.modules.identity.adapter.in.web.shared;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
+
+import java.time.OffsetDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ResponseHTTP<T>(
+        T data,
+        String message,
+        boolean success,
+        String traceId,
+        OffsetDateTime timestamp
+) {
+
+    public ResponseHTTP {
+        if (timestamp == null) {
+            timestamp = OffsetDateTime.now();
+        }
+        if (traceId == null) {
+            traceId = resolveCurrentTraceId();
+        }
+    }
+
+    public static <T> ResponseHTTP<T> ok(T data) {
+        return new ResponseHTTP<>(data, "Success", true, null, null);
+    }
+
+    public static <T> ResponseHTTP<T> okTraceId(String id) {
+        return new ResponseHTTP<>(null, "Success", true, id, null);
+    }
+
+    public static <T> ResponseHTTP<T> ok(T data, String traceId) {
+        return new ResponseHTTP<>(data, null, true, traceId, null);
+    }
+
+    public static <T> ResponseHTTP<T> ok(T data, String traceId, String message) {
+        return new ResponseHTTP<>(data, message, true, traceId, null);
+    }
+
+    public static <T> ResponseHTTP<T> error(String message) {
+        return new ResponseHTTP<>(null, message, false, null, null);
+    }
+
+    public static <T> ResponseHTTP<T> error(String message, String customTraceId) {
+        return new ResponseHTTP<>(null, message, false, customTraceId, null);
+    }
+
+    private static String resolveCurrentTraceId() {
+        SpanContext spanContext = Span.current().getSpanContext();
+        if (spanContext.isValid()) {
+            return spanContext.getTraceId();
+        }
+        return null;
+    }
+}
