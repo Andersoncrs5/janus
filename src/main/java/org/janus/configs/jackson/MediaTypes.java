8a11f167dd53273b011826beaad2308f207e2dd0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-22 10:38:53 -0300

    New class with MediaType

---
+++ src/main/java/org/janus/configs/jackson/MediaTypes.java
@@ -0,0 +1,32 @@
+package org.janus.configs.jackson;
+
+public class MediaTypes {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String APPLICATION_YAML = "application/yaml";
+    public static final String APPLICATION_PDF = "application/pdf";
+    public static final String APPLICATION_ZIP = "application/zip";
+    public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
+    public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    public static final String TEXT_PLAIN = "text/plain";
+    public static final String TEXT_HTML = "text/html";
+    public static final String TEXT_CSV = "text/csv";
+    public static final String TEXT_XML = "text/xml";
+    public static final String TEXT_EVENT_STREAM = "text/event-stream";
+
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+
+    public static final String IMAGE_PNG = "image/png";
+    public static final String IMAGE_JPEG = "image/jpeg";
+    public static final String IMAGE_GIF = "image/gif";
+    public static final String IMAGE_SVG = "image/svg+xml";
+
+    public static final String AUDIO_MPEG = "audio/mpeg";
+    public static final String AUDIO_OGG = "audio/ogg";
+
+    public static final String VIDEO_MP4 = "video/mp4";
+    public static final String VIDEO_WEBM = "video/webm";
+
+}
