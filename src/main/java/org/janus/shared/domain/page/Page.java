74d73e3a75c1ba7343cd31c586c4888a159791ba
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-22 10:39:00 -0300

    Set up Page

---
+++ src/main/java/org/janus/shared/domain/page/Page.java
@@ -0,0 +1,45 @@
+package org.janus.shared.domain.page;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Page<T> {
+
+    private List<T> content;
+    private long totalElements;
+    private int page;
+    private int size;
+    private int totalPages;
+
+    private boolean first;
+    private boolean last;
+
+    public Page(
+            List<T> content,
+            long totalElements,
+            int page,
+            int size
+    ) {
+        this.content = content;
+        this.totalElements = totalElements;
+        this.page = page;
+        this.size = size;
+
+        this.totalPages = size > 0
+                ? (int) Math.ceil((double) totalElements / size)
+                : 0;
+
+        this.first = page == 0;
+        this.last = page >= totalPages - 1;
+    }
+
+    public boolean hasNext() {
+        return page + 1 < totalPages;
+    }
+
+    public boolean hasPrevious() {
+        return page > 0;
+    }
+}
