4ecc4aea8b8658e7f1034d52b6d7e7e17aacfbd1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-25 20:31:39 -0300

    New methods

--- src/main/java/org/janus/modules/authorization/infrastructure/out/UserRoleRepository.java
+++ src/main/java/org/janus/modules/authorization/infrastructure/out/UserRoleRepository.java
@@ -3,8 +3,30 @@ package org.janus.modules.authorization.infrastructure.out;
 import org.janus.modules.authorization.domain.entity.UserRoleEntity;
 import org.janus.shared.domain.base.repository.GenericRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRoleRepository extends GenericRepository<UserRoleEntity, UUID> {
+    boolean existsByUserIdAndRoleId(UUID userId, UUID roleId);
+    List<UUID> findAllRoleIdsByUserId(UUID id);
 
+    int deleteByUserIdAndRoleId(
+            UUID userId,
+            UUID roleId
+    );
+
+    int deleteAllByUserId(
+            UUID userId
+    );
+
+    boolean existsActiveByUserIdAndRoleId(
+            UUID userId,
+            UUID roleId
+    );
+
+    Optional<UserRoleEntity> findByUserIdAndRoleId(
+            UUID userId,
+            UUID roleId
+    );
 }
