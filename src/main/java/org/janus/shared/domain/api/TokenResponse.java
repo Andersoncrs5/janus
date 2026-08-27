bceeb0d07213bde183ba91a286ac65efd79e89c6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-08-27 15:00:57 -0300

    Set up TokenResponse

---
+++ src/main/java/org/janus/shared/domain/api/TokenResponse.java
@@ -0,0 +1,24 @@
+package org.janus.shared.domain.api;
+
+import jakarta.validation.constraints.NotBlank;
+import org.janus.modules.identity.application.user.dto.UserDTO;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+public record TokenResponse(
+        @NotBlank
+        String token,
+
+        OffsetDateTime expToken,
+
+        @NotBlank
+        String refreshToken,
+
+        OffsetDateTime expRefreshToken,
+
+        UserDTO user,
+
+        List<String> roles
+) {
+}
