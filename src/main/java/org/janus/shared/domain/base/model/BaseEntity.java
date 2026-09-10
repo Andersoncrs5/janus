163e6859221b734d605a93848d9fa2df4ce32dd5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-10 20:19:13 -0300

    New method and validations

--- src/main/java/org/janus/shared/domain/base/model/BaseEntity.java
+++ src/main/java/org/janus/shared/domain/base/model/BaseEntity.java
@@ -1,13 +1,11 @@
 package org.janus.shared.domain.base.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.jspecify.annotations.Nullable;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -15,21 +13,54 @@ import java.util.UUID;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class BaseEntity {
+public abstract class BaseEntity {
 
     protected UUID id;
+
+    @Setter(AccessLevel.PROTECTED)
     protected Long version;
+
+    @Setter(AccessLevel.PROTECTED)
     protected OffsetDateTime createdAt;
+
+    @Setter(AccessLevel.PROTECTED)
     protected OffsetDateTime updatedAt;
+
     @Nullable
+    @Setter(AccessLevel.PROTECTED)
     protected OffsetDateTime deletedAt;
 
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+
     public void markAsDeleted() {
-        setDeletedAt(OffsetDateTime.now());
+        this.deletedAt = OffsetDateTime.now();
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+    public void touch() {
+        this.updatedAt = OffsetDateTime.now();
     }
 
     public void incrementVersion() {
-        this.version = (this.version == null ? 0L : this.version) + 1L;
+        this.version = (this.version == null) ? 1L : this.version + 1L;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
