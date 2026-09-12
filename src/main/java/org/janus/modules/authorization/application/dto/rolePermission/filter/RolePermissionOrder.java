9c940d2f95710bd45e1008435f0637bf590c8999
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-12 18:21:03 -0300

    Remake

--- src/main/java/org/janus/modules/authorization/application/dto/rolePermission/filter/RolePermissionOrder.java
+++ src/main/java/org/janus/modules/authorization/application/dto/rolePermission/filter/RolePermissionOrder.java
@@ -4,19 +4,21 @@ import lombok.Getter;
 
 @Getter
 public enum RolePermissionOrder {
-    ID("id"),
-    ROLE_ID("role_id"),
-    PERMISSION_ID("permission_id"),
-    EFFECT("effect"),
-    EXPIRES_AT("expires_at"),
-    ASSIGNED_AT("assigned_at"),
-    VERSION("version"),
-    CREATED_AT("created_at"),
-    UPDATED_AT("updated_at");
+    ID("id", "ASC"),
+    ROLE_ID("role_id", "ASC"),
+    PERMISSION_ID("permission_id", "ASC"),
+    EFFECT("effect", "ASC"),
+    EXPIRES_AT("expires_at", "DESC"),
+    ASSIGNED_AT("assigned_at", "DESC"),
+    VERSION("version", "ASC"),
+    CREATED_AT("created_at", "DESC"),
+    UPDATED_AT("updated_at", "DESC");
 
-    private final String field;
+    private final String column;
+    private final String direction;
 
-    RolePermissionOrder(String field) {
-        this.field = field;
+    RolePermissionOrder(String column, String direction) {
+        this.column = column;
+        this.direction = direction;
     }
 }
