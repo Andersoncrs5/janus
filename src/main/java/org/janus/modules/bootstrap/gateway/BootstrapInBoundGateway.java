a5d3b84b1b154b76ca6bfb0a7208732a25320eef
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-12 18:19:54 -0300

    Added PermissionOutboundGateway

--- src/main/java/org/janus/modules/bootstrap/gateway/BootstrapInBoundGateway.java
+++ src/main/java/org/janus/modules/bootstrap/gateway/BootstrapInBoundGateway.java
@@ -1,11 +1,12 @@
 package org.janus.modules.bootstrap.gateway;
 
+import jakarta.enterprise.context.Dependent;
+import org.janus.modules.authorization.gateway.permission.PermissionOutboundGateway;
 import org.janus.modules.authorization.gateway.role.RoleOutboundGateway;
 import org.janus.modules.authorization.gateway.userRole.UserRoleOutBoundGateway;
 import org.janus.modules.authorization.infrastructure.in.role.ICreateRoleUseCase;
 import org.janus.modules.authorization.infrastructure.in.role.IExistsRoleByNameUseCase;
 import org.janus.modules.identity.gateway.user.UserOutboundGateway;
-import jakarta.enterprise.context.Dependent;
 
 @Dependent
 public record BootstrapInBoundGateway(
@@ -16,6 +17,8 @@ public record BootstrapInBoundGateway(
 
         RoleOutboundGateway roleOutboundGateway,
 
-        UserRoleOutBoundGateway userRoleOutBoundGateway
+        UserRoleOutBoundGateway userRoleOutBoundGateway,
+
+        PermissionOutboundGateway permissionOutboundGateway
 ) {
 }
