bbabefd6ae7107cbc07efaa77c1d998fc4efac7b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-17 18:04:38 -0300

    Added another bootstrap

--- src/main/java/org/janus/modules/bootstrap/ApplicationBootstrap.java
+++ src/main/java/org/janus/modules/bootstrap/ApplicationBootstrap.java
@@ -4,25 +4,33 @@ import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.jbosslog.JBossLog;
+import org.janus.modules.bootstrap.service.permission.CreatePermissionUseCaseBootstrap;
 import org.janus.modules.bootstrap.service.role.CreateRolesUseCase;
+import org.janus.modules.bootstrap.service.rolePermission.CreateRolePermissionUseCaseBootstrap;
 import org.janus.modules.bootstrap.service.user.CreateUserMasterUseCase;
 import org.janus.modules.bootstrap.service.userRole.LinkRoleMasterToUserMasterUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@JBossLog
 @ApplicationScoped
 @RequiredArgsConstructor
 public class ApplicationBootstrap {
 
+    private static final Logger log = LoggerFactory.getLogger(ApplicationBootstrap.class);
+
     private final CreateUserMasterUseCase createMasterUser;
     private final CreateRolesUseCase createRolesUseCase;
+    private final CreatePermissionUseCaseBootstrap createPermissionUseCase;
     private final LinkRoleMasterToUserMasterUseCase linkRoleMasterToUserMasterUseCase;
+    private final CreateRolePermissionUseCaseBootstrap createRolePermissionUseCase;
 
     public void onStart(@Observes StartupEvent event) {
         log.info("Starting application data bootstrap...");
 
         createMasterUser.execute();
+        createPermissionUseCase.execute();
         createRolesUseCase.execute();
+        createRolePermissionUseCase.execute();
         linkRoleMasterToUserMasterUseCase.execute();
 
         log.info("Application data bootstrap completed successfully.");
