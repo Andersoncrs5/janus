3df62e1a6ecf0a09c81145f0b831fcdbc1e33def
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-17 20:51:31 -0300

    Added new column 'username'

---
+++ src/main/java/org/janus/modules/identity/application/user/dto/request/UpdateUserDTO.java
@@ -0,0 +1,14 @@
+package org.janus.modules.identity.application.user.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserDTO {
+    private String 'username';
+    private String fullName;
+    private Boolean isActive;
+}
