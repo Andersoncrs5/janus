21db8c15135f6302ebf12857d110a5c9483736d9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-09-24 19:41:09 -0300

    Added getters and setters

--- src/main/java/org/janus/modules/authorization/application/dto/rolePermission/response/RolePermissionDTO.java
+++ src/main/java/org/janus/modules/authorization/application/dto/rolePermission/response/RolePermissionDTO.java
@@ -11,8 +11,6 @@ import org.janus.shared.domain.enums.rolePermission.PermissionEffectEnum;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-@Getter
-@Setter
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,4 +23,60 @@ public class RolePermissionDTO extends BaseDTO {
     private OffsetDateTime expiresAt;
     private UUID assignedBy;
     private OffsetDateTime assignedAt;
+
+    public UUID getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(UUID roleId) {
+        this.roleId = roleId;
+    }
+
+    public UUID getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(UUID permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public PermissionEffectEnum getEffect() {
+        return effect;
+    }
+
+    public void setEffect(PermissionEffectEnum effect) {
+        this.effect = effect;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(String conditions) {
+        this.conditions = conditions;
+    }
+
+    public OffsetDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(OffsetDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public UUID getAssignedBy() {
+        return assignedBy;
+    }
+
+    public void setAssignedBy(UUID assignedBy) {
+        this.assignedBy = assignedBy;
+    }
+
+    public OffsetDateTime getAssignedAt() {
+        return assignedAt;
+    }
+
+    public void setAssignedAt(OffsetDateTime assignedAt) {
+        this.assignedAt = assignedAt;
+    }
 }
